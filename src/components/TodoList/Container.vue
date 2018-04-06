e0db58270d905d4ccd944f232f652614951b41d5
Jimmy Liao <gooyumi@gmail.com>
2018-04-06 14:57:10 +0800

    Add el-header

--- src/components/TodoList/Container.vue
+++ src/components/TodoList/Container.vue
@@ -1,8 +1,20 @@
 <template>
-  <p>Container</p>
+  <el-container>
+    <el-header/>
+    <el-main/>
+  </el-container>
 </template>
 
 <script>
 export default {};
 </script>
 
+<style scoped>
+.el-header {
+  background-color: #46A0FC;
+  color: #333;
+  position: fixed;
+  width: 100%;
+  height: 50px;
+}
+</style>
--- src/helper/ElementUI.js
+++ src/helper/ElementUI.js
@@ -1,7 +1,13 @@
-import { Button } from 'element-ui';
+import {
+  Button,
+  Container,
+  Header,
+} from 'element-ui';
 
 export const importer = (vue) => {
   vue.use(Button);
+  vue.use(Container);
+  vue.use(Header);
 };
 
 export default {
