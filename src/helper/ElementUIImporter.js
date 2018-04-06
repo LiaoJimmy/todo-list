b867c1016fd7ccca8fb8a2511b0923e924c4020b
Jimmy Liao <gooyumi@gmail.com>
2018-04-06 10:01:18 +0800

    Add element UI

---
+++ src/helper/ElementUIImporter.js
@@ -0,0 +1,7 @@
+import { Button } from 'element-ui';
+
+export const ElementUI = (vue) => {
+  vue.use(Button);
+};
+
+export default ElementUI;
--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 
+import ElementUIImporter from './helper/ElementUIImporter';
+
+ElementUIImporter(Vue);
+
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
